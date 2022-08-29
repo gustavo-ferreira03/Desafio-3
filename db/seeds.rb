7c19b8e9a873aae58a3403bafd5337c77275b353
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-08-29 19:54:45 -0300

    feat: added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+csv = CSV.foreach(Rails.root.join('lib', 'seeds', 'notas.csv'), headers: true) do |t|
+    if !Student.find_by(id: t["MATRICULA"].to_i) 
+        puts "Creating student #{t["MATRICULA"]}"
+        student = Student.create(id: t["MATRICULA"].to_i, course_id: t["COD_CURSO"].to_i)
+    end
+    if !Course.find_by(id: t["COD_CURSO"].to_i) 
+        puts "Creating course #{t["COD_CURSO"]}"
+        course = Course.create(id: t["COD_CURSO"].to_i)
+    end
+    if !Subject.find_by(code: t["COD_DISCIPLINA"])
+        puts "Creating subject #{t["COD_DISCIPLINA"]}"
+        subject = Subject.create(code: t["COD_DISCIPLINA"])
+    end
+
+    puts "Creating attended subject(#{t["COD_DISCIPLINA"]}) for student #{t["MATRICULA"]} | grade: #{t["NOTA"]} | period: #{t["ANO_SEMESTRE"]}"
+    AttendedSubject.create(
+        student_id: t["MATRICULA"],
+        subject_id: Subject.find_by(code: t["COD_DISCIPLINA"]).id,
+        grade: t["NOTA"],
+        period: t["ANO_SEMESTRE"]
+    )
+end
