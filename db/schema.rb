2429fde43c93f2d4badbcd7371c720eeafec26f1
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-08-29 00:20:45 -0300

    added student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_020543) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_031932) do
   create_table "courses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "students", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_students_on_course_id"
+  end
+
+  add_foreign_key "students", "courses"
 end
