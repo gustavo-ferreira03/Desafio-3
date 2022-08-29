ecc6fde0d3b1a68bbb7df010898475334df689f2
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-08-29 19:11:01 -0300

    test: added course average cr test case

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
     factory :course do
+        trait :with_students do
+            transient do
+                students_count { 5 }
+            end
+    
+            after(:create) do |course, evaluator|
+                create_list(:student, evaluator.students_count, course: course)
+            end
+        end
     end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
+  subject { create(:course, :with_students) }
+
   describe "associations" do
     it { should have_many(:students) }
+  end
+
+  describe "#average_cr" do
+    it "returns the average cr" do
+      expect(subject.average_cr).to be_kind_of(Integer)
+      expect(subject.average_cr)
+    end
   end
 end
