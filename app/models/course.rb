3d7db35d47e79dd2117b0e4652288b0eafc1250d
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-08-29 19:27:05 -0300

    feat: added average_cr method for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,11 @@
 class Course < ApplicationRecord
     has_many :students
+
+    def average_cr
+        sum = 0
+        students.each do |student|
+            sum += student.cr
+        end
+        sum / students.count
+    end
 end
